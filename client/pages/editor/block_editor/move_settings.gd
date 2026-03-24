6913c8206b87e3ac7143b46590afbf32d6e715bf
Jeremiah <jqrqmiqh@gmail.com>
2026-03-24 01:18:19 -0400

    mostly work on move settings
    - some changes to change settings
    - implementing move settings at the moment. right now you can parse your move patterns to see if they are valid (randomize and loop buttons aren't implemented yet.)

--- client/pages/editor/block_editor/move_settings.gd
+++ client/pages/editor/block_editor/move_settings.gd
@@ -1,1 +1,43 @@
 extends Control
+
+@onready var tick_box = $TickBox
+@onready var pattern_box = $PatternBox
+@onready var parse_pattern_button = $ParsePatternButton
+@onready var parse_results_label = $ParseResultsLabel
+
+var allowed_commands: Array = ["up", "u", "^", "down", "d", "v", "left", "l", "<", "right", "r", ">", "random", "return"]
+
+
+func _ready() -> void:
+	tick_box.init("float", "2.5", 0.0, 99999999.9)
+	parse_pattern_button.pressed.connect(_parse_pattern)
+	_parse_pattern()
+
+
+func _parse_pattern():
+	var success: bool = true
+	parse_results_label.text = ""
+	var pattern_string = pattern_box.text
+	if !pattern_string.is_empty():
+		var pattern_array = pattern_string.split(",", false, 0)
+		for pattern in pattern_array:
+			if pattern.dedent().rstrip("0123456789 ").to_lower() in allowed_commands:
+				if pattern.contains("0123456789"):
+					if pattern.dedent().rstrip("up^downvleft<righ>am ").is_valid_int():
+						var number = pattern.dedent().rstrip("up^downvleft<righ>am ")
+					else:
+						success = false
+						parse_results_label.set("theme_override_colors/default_color", Color("7F0000"))
+						parse_results_label.text = "ERROR: One or more invalid numerations."
+			else:
+				success = false
+				parse_results_label.set("theme_override_colors/default_color", Color("7F0000"))
+				parse_results_label.text = "ERROR: One or more invalid commands."
+				break
+	else:
+		success = false
+		parse_results_label.set("theme_override_colors/default_color", Color("7F0000"))
+		parse_results_label.text = "ERROR: No pattern exists."
+	if success:
+		parse_results_label.set("theme_override_colors/default_color", Color("007f00"))
+		parse_results_label.text = "PASS: All patterns are valid."
